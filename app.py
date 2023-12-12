6bad16a74101a30caa8574e82d1b45415fdff62e
unknown <asm21612@gmail.com>
2023-12-12 13:57:37 +0600

    Basic structure

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return render_template('index.html', error='No file part')
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return render_template('index.html', error='No selected file')
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        df = pd.read_csv(filepath)
+
+        return render_template('dashboard.html', table=df.head().to_html(), filename=filename)
+
+    else:
+        return render_template('index.html', error='Invalid file type')
+
+if __name__ == '__main__':
+    app.run(debug=True)
